324d982ce111f4af8fd19904c96577f06fb38730
Jesus Perez <jesush.prz@gmail.com>
2019-11-17 18:25:13 -0600

    adds Note model

---
+++ src/main/java/com/jesushperez/notes/model/Note.java
@@ -0,0 +1,58 @@
+package com.jesushperez.notes.model;
+
+public class Note {
+
+    private String id;
+    private String date;
+    private String title;
+    private String content;
+    private boolean isPinned;
+
+    public Note(String id, String date, String title, String content, boolean isPinned) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.content = content;
+        this.isPinned = isPinned;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+}
