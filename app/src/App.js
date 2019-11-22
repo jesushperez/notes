c51a5dc4ee8bcef08ec9bb7f3622233f31910d84
Jesus Perez <jesush.prz@gmail.com>
2019-11-21 19:42:07 -0600

    renders sample notes from json server

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
    BrowserRouter as Router,
    Switch, Route
@@ -7,36 +8,16 @@ import HomePage from './components/HomePage';
 import './base.css';
 
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: "1",
-      date: "May 11, 2018",
-      title: "Note 1 title",
-      content: "Note 1 content.",
-      isPinned: true
-    },
-    {
-      id: "2",
-      date: "June 23, 2019",
-      title: "Note 2 title",
-      content: "Note 2 content.",
-      isPinned: true
-    },
-    {
-      id: "3",
-      date: "December 13, 2019",
-      title: "Note 3 title",
-      content: "Note 3 content.",
-      isPinned: false
-    },
-    {
-      id: "4",
-      date: "April 24, 2020",
-      title: "Note Four Has a Long Title Than the Other Four",
-      content: "Bacon ipsum dolor amet tongue capicola landjaeger pig, prosciutto shank bacon beef hamburger pastrami. Jowl biltong strip steak swine sausage capicola.",
-      isPinned: true
-    }
-  ]);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const serverUrl = 'http://localhost:3001/notes';
+    axios
+      .get(serverUrl)
+      .then(response => {
+        setNotes(response.data);
+      })
+  }, []);
 
   return (
     <div>
